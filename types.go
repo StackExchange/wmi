e3c867b04a972730cbc4ee30b0c1f2c0865dd893
Kyle Brandt <kyle@kbrandt.com>
2013-12-06 11:33:05 -0500

    Add Win32_OperatingSystem Type to get simple memory stats

--- types.go
+++ types.go
@@ -190,3 +190,70 @@ type Win32_PerfRawData_PerfDisk_PhysicalDisk struct {
 	Timestamp_PerfTime           uint64
 	Timestamp_Sys100NS           uint64
 }
+
+type Win32_OperatingSystem struct {
+	BootDevice                                string
+	BuildNumber                               string
+	BuildType                                 string
+	Caption                                   string
+	CodeSet                                   string
+	CountryCode                               string
+	CreationClassName                         string
+	CSCreationClassName                       string
+	CSDVersion                                string
+	CSName                                    string
+	CurrentTimeZone                           int16
+	DataExecutionPrevention_Available         bool
+	DataExecutionPrevention_32BitApplications bool
+	DataExecutionPrevention_Drivers           bool
+	DataExecutionPrevention_SupportPolicy     uint8
+	Debug                                     bool
+	Description                               string
+	Distributed                               bool
+	EncryptionLevel                           uint32
+	ForegroundApplicationBoost                uint8
+	FreePhysicalMemory                        uint64
+	FreeSpaceInPagingFiles                    uint64
+	FreeVirtualMemory                         uint64
+	InstallDate                               time.Time
+	LargeSystemCache                          uint32
+	LastBootUpTime                            time.Time
+	LocalDateTime                             time.Time
+	Locale                                    string
+	Manufacturer                              string
+	MaxNumberOfProcesses                      uint32
+	MaxProcessMemorySize                      uint64
+	MUILanguages                              []string
+	Name                                      string
+	NumberOfLicensedUsers                     uint32
+	NumberOfProcesses                         uint32
+	NumberOfUsers                             uint32
+	OperatingSystemSKU                        uint32
+	Organization                              string
+	OSArchitecture                            string
+	OSLanguage                                uint32
+	OSProductSuite                            uint32
+	OSType                                    uint16
+	OtherTypeDescription                      string
+	PAEEnabled                                bool
+	PlusProductID                             string
+	PlusVersionNumber                         string
+	PortableOperatingSystem                   bool
+	Primary                                   bool
+	ProductType                               uint32
+	RegisteredUser                            string
+	SerialNumber                              string
+	ServicePackMajorVersion                   uint16
+	ServicePackMinorVersion                   uint16
+	SizeStoredInPagingFiles                   uint64
+	Status                                    string
+	SuiteMask                                 uint32
+	SystemDevice                              string
+	SystemDirectory                           string
+	SystemDrive                               string
+	TotalSwapSpaceSize                        uint64
+	TotalVirtualMemorySize                    uint64
+	TotalVisibleMemorySize                    uint64
+	Version                                   string
+	WindowsDirectory                          string
+}
