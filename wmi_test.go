6cdeeeedec1e81d11c5e219ca41eb5e73dbfe64a
Matt Jibson <matt.jibson@gmail.com>
2013-12-09 19:55:21 -0500

    Remove types from this package

--- wmi_test.go
+++ wmi_test.go
@@ -16,9 +16,57 @@ func TestFieldMismatch(t *testing.T) {
 		HandleCount uint32
 		Blah        uint32
 	}
-	var dst[]s
+	var dst []s
 	err := Query("SELECT Name, HandleCount FROM Win32_Process", &dst)
 	if err == nil || err.Error() != `wmi: cannot load field "Blah" into a "uint32": no such struct field` {
 		t.Error("Expected err field mismatch")
 	}
+}
+
+type Win32_Process struct {
+	CSCreationClassName        string
+	CSName                     string
+	Caption                    string
+	CommandLine                string
+	CreationClassName          string
+	CreationDate               time.Time
+	Description                string
+	ExecutablePath             string
+	ExecutionState             uint16
+	Handle                     string
+	HandleCount                uint32
+	InstallDate                time.Time
+	KernelModeTime             uint64
+	MaximumWorkingSetSize      uint32
+	MinimumWorkingSetSize      uint32
+	Name                       string
+	OSCreationClassName        string
+	OSName                     string
+	OtherOperationCount        uint64
+	OtherTransferCount         uint64
+	PageFaults                 uint32
+	PageFileUsage              uint32
+	ParentProcessId            uint32
+	PeakPageFileUsage          uint32
+	PeakVirtualSize            uint64
+	PeakWorkingSetSize         uint32
+	Priority                   uint32
+	PrivatePageCount           uint64
+	ProcessId                  uint32
+	QuotaNonPagedPoolUsage     uint32
+	QuotaPagedPoolUsage        uint32
+	QuotaPeakNonPagedPoolUsage uint32
+	QuotaPeakPagedPoolUsage    uint32
+	ReadOperationCount         uint64
+	ReadTransferCount          uint64
+	SessionId                  uint32
+	Status                     string
+	TerminationDate            time.Time
+	ThreadCount                uint32
+	UserModeTime               uint64
+	VirtualSize                uint64
+	WindowsVersion             string
+	WorkingSetSize             uint64
+	WriteOperationCount        uint64
+	WriteTransferCount         uint64
 }
