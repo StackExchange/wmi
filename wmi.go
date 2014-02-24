085b191bad15e6b5fd16365b81541f8e0a9d6cb0
Matt Jibson <matt.jibson@gmail.com>
2014-02-24 15:50:03 -0500

    More yelling about the memory bug

--- wmi.go
+++ wmi.go
@@ -1,4 +1,14 @@
-// Package wmi provides a WQL interface for WMI on Windows.
+/*
+Package wmi provides a WQL interface for WMI on Windows.
+
+Warning
+
+Due to a memory bug (https://github.com/mattn/go-ole/issues/13) only use the wmi
+package in its own executable and never within a package. This executable must
+disable the GC and should either exit after running a query or safely exit after
+running N queries (since memory use will go up). See this github issue
+(https://github.com/StackExchange/wmi/issues/1) for more details.
+*/
 package wmi
 
 import (
