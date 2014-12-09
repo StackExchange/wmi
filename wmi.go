426decf6908cf5221297f4d73419236e67c3f85d
Greg Bray <Greg.Bray@Gmail.com>
2014-12-08 23:15:22 -0700

    First whack at wmi InvokeMethod

--- wmi.go
+++ wmi.go
@@ -46,6 +46,121 @@ var (
 	lock                 sync.Mutex
 )
 
+func InvokeMethod(class string, name string, args []interface{}, dst interface{}, connectServerArgs ...interface{}) error {
+	dv := reflect.ValueOf(dst)
+	fmt.Println()
+	if dv.Kind() != reflect.Ptr || dv.IsNil() {
+		return ErrInvalidEntityType
+	}
+	elemType := dv.Type().Elem()
+	//if mat == multiArgTypeInvalid {
+	//	return ErrInvalidEntityType
+	//}
+
+	lock.Lock()
+	defer lock.Unlock()
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	err := ole.CoInitializeEx(0, ole.COINIT_MULTITHREADED)
+	if err != nil {
+		oleerr := err.(*ole.OleError)
+		// S_FALSE           = 0x00000001 // CoInitializeEx was already called on this thread
+		if oleerr.Code() != ole.S_OK && oleerr.Code() != 0x00000001 {
+			l.Printf("CoInitializeEx err %v\n", oleerr)
+			return err
+		}
+	} else {
+		// Only invoke CoUninitialize if the thread was not initizlied before.
+		// This will allow other go packages based on go-ole play along
+		// with this library.
+
+		//This was causing "Object is not connected to server." issues with ConfigurationModeFrequencyMins. Need to know how to initilize this outside the method or transform variant to native struct before returning
+		//defer ole.CoUninitialize()
+	}
+
+	unknown, err := oleutil.CreateObject("WbemScripting.SWbemLocator")
+	if err != nil {
+		l.Printf("SWbemLocator err %v\n", err)
+		return err
+	}
+	defer unknown.Release()
+
+	wmi, err := unknown.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		l.Printf("SWbemLocator IID_IDispatch err %v\n", err)
+		return err
+	}
+	defer wmi.Release()
+
+	// service is a SWbemServices
+	serviceRaw, err := oleutil.CallMethod(wmi, "ConnectServer", connectServerArgs...)
+	if err != nil {
+		l.Printf("ConnectServer err %v\n", err)
+		return err
+	}
+	service := serviceRaw.ToIDispatch()
+	defer serviceRaw.Clear()
+
+	// wmiClass is a SWbemObject
+	l.Printf("Get wmiClass: %v\n", class)
+	wmiClassRaw, err := oleutil.CallMethod(service, "Get", class)
+	if err != nil {
+		l.Printf("Get err %v\n", err)
+		return err
+	}
+	wmiClass := wmiClassRaw.ToIDispatch()
+	defer wmiClassRaw.Clear()
+
+	if wmiClass == nil {
+		l.Panicf("ERROR: wmiClass is nil!\n")
+	}
+
+	// result is
+	l.Printf("call method '%v' on wmiClass %v with args: %v\n", name, wmiClass, args)
+	resultRaw, err := oleutil.CallMethod(wmiClass, name, args...)
+	//resultRaw, err := oleutil.CallMethod(wmiClass, "ExecMethod_", name)
+	if err != nil {
+		l.Printf("wmiClass method err %v\n", err)
+		return err
+	}
+	result := resultRaw.Value()
+	defer resultRaw.Clear()
+	vresult := reflect.ValueOf(result)
+	tvresult := vresult.Type()
+	if tvresult == elemType {
+		dv.Elem().Set(vresult)
+		//TODO: use code below to convert output ole.VARIANT type to go struct.
+		return nil
+	} else {
+		return errors.New(fmt.Sprintf("type %v returned from %v method does not match dst type %v", tvresult, name, elemType))
+	}
+
+	/*
+			l.Printf("result pointer is %p", result)
+
+			l.Printf("use loadEntity to convert result %p to elemType: %v\n", result, elemType)
+			var errFieldMismatch error
+			ev := reflect.New(elemType)
+			l.Printf("ev: %v\n", ev)
+			if err = loadEntity(ev.Interface(), result); err != nil {
+				if _, ok := err.(*ErrFieldMismatch); ok {
+					l.Println("3")
+					// We continue loading entities even in the face of field mismatch errors.
+					// If we encounter any other error, that other error is returned. Otherwise,
+					// an ErrFieldMismatch is returned.
+					errFieldMismatch = err
+				} else {
+					l.Println("4")
+					return err
+				}
+			}
+			l.Println("5")
+
+		return errFieldMismatch
+	*/
+}
+
 // QueryNamespace invokes Query with the given namespace on the local machine.
 func QueryNamespace(query string, dst interface{}, namespace string) error {
 	return Query(query, dst, nil, namespace)
@@ -139,6 +254,7 @@ func Query(query string, dst interface{}, connectServerArgs ...interface{}) erro
 			defer itemRaw.Clear()
 
 			ev := reflect.New(elemType)
+			//l.Printf("use loadEntity to convert item %v to elemType: %v\n", item, elemType)
 			if err = loadEntity(ev.Interface(), item); err != nil {
 				if _, ok := err.(*ErrFieldMismatch); ok {
 					// We continue loading entities even in the face of field mismatch errors.
@@ -182,6 +298,7 @@ var timeType = reflect.TypeOf(time.Time{})
 // loadEntity loads a SWbemObject into a struct pointer.
 func loadEntity(dst interface{}, src *ole.IDispatch) (errFieldMismatch error) {
 	v := reflect.ValueOf(dst).Elem()
+	l.Printf("v: %v\n", v)
 	for i := 0; i < v.NumField(); i++ {
 		f := v.Field(i)
 		isPtr := f.Kind() == reflect.Ptr
@@ -190,15 +307,20 @@ func loadEntity(dst interface{}, src *ole.IDispatch) (errFieldMismatch error) {
 			f.Set(ptr)
 			f = f.Elem()
 		}
+		l.Printf("f(%v): %v\n", i, f)
 		n := v.Type().Field(i).Name
+		l.Printf("n(%v): %v\n", i, n)
 		if !f.CanSet() {
+			l.Printf("!f.CanSet()\n")
 			return &ErrFieldMismatch{
 				StructType: f.Type(),
 				FieldName:  n,
 				Reason:     "CanSet() is false",
 			}
 		}
+		l.Printf("next\n")
 		prop, err := oleutil.GetProperty(src, n)
+		l.Printf("prop: %v, err: %v\n", prop, err)
 		if err != nil {
 			errFieldMismatch = &ErrFieldMismatch{
 				StructType: f.Type(),
--- wmi_test.go
+++ wmi_test.go
@@ -1,15 +1,167 @@
 package wmi
 
 import (
-	"encoding/json"
-	"fmt"
-	"reflect"
-	"runtime"
-	"runtime/debug"
-	"sync"
 	"testing"
-	"time"
+
+	"github.com/mattn/go-ole"
+	"github.com/mattn/go-ole/oleutil"
 )
+
+//23456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+func TestInvokeProcess(t *testing.T) {
+	var err error
+	var args []interface{}
+	/*err := InvokeMethod("fakeclass", "fakemethod", nil)
+			if err == nil {
+				t.Fatal("Expected error when invoking fakemethod on fakeclass")
+			}
+
+			err := InvokeMethod("StdRegProv", "GetStringValue", nil)
+			if err != nil {
+			t.Fatal(err)
+		}
+
+		//Powershell testing
+		#gwmi StdRegProv -List | select -ExpandProperty Methods
+		$HKCU = [uint32]'0x80000001'
+		$HKLM = [uint32]'0x80000002'
+		$classREG = [wmiclass]"StdRegProv"
+		#$classREG.EnumValues($HKLM,"System\CurrentControlSet\Control\Session Manager\Environment")
+		$classREG.GetStringValue($HKLM,"System\CurrentControlSet\Control\Session Manager\Environment","Path")
+
+
+		#gwmi Win32_Process -List | select -ExpandProperty Methods
+		$classPS = [wmiclass]"Win32_Process"
+		$result = $classPS.Create("cmd.exe",$null,$null)
+		$result | gm *
+
+		$classSVC = gwmi -class Win32_Service -Filter "Name='WSearch'"
+		$classSVC.StartService() | GM *
+
+	  ////using new CIM methods in WMF 4/5
+		//$dsclcm = Get-CimClass -Namespace ROOT\Microsoft\Windows\DesiredStateConfiguration MSFT_DSCLocalConfigurationManager
+		//icim -CimClass $dsclcm -MethodName GetConfigurationStatus -ComputerName localhost | select -ExpandProperty ConfigurationStatus
+
+	  //using wmi methods
+		$dsc = gwmi *DSC* -list -Namespace root\Microsoft\Windows\DesiredStateConfiguration
+		$dsclcm = @($dsc | ?{$_.Name -eq 'MSFT_DSCLocalConfigurationManager'})[0]
+		$dsclcm | gm * -force
+
+		$result = $dsclcm.GetMetaConfiguration()
+		$result.MetaConfiguration
+
+		$result = $dsclcm.GetConfigurationStatus(0)
+		$result.ConfigurationStatus
+
+	*/
+
+	//NOTE: These require running 'go test' as an administrator, otherwise you will get access denied errors.
+
+	var ReturnValue int = -100 //see http://msdn.microsoft.com/en-us/library/aa393673(v=vs.85).aspx
+	/*
+		args = []interface{}{}
+		err = InvokeMethod("Win32_Service.Name='Audiosrv'", "StopService", args, &ReturnValue)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("StopService Result: %v", ReturnValue)
+
+		time.Sleep(2 * time.Second) //Increase delay to confirm service is stopped
+
+		ReturnValue = -100 //see http://msdn.microsoft.com/en-us/library/aa393660(v=vs.85).aspx
+		args = []interface{}{}
+		err = InvokeMethod("Win32_Service.Name='Audiosrv'", "StartService", args, &ReturnValue)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("StartService Result: %v", ReturnValue)
+
+		ReturnValue = -100 //see http://msdn.microsoft.com/en-us/library/aa389388(v=vs.85).aspx
+		var ProcessID int
+		//todo: replace nil with Win32_ProcessStartup class http://msdn.microsoft.com/en-us/library/aa394375(v=vs.85).aspx
+		//also find a way to pass parameters to program. ex: cmd.exe /C echo hello
+		args = []interface{}{"cmd.exe", "c:\\", nil, &ProcessID}
+		err = InvokeMethod("Win32_Process", "Create", args, &ReturnValue)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Create Process Result: %v ProcessID: %v", ReturnValue, ProcessID)
+
+		time.Sleep(2 * time.Second)
+
+		ReturnValue = -100     //see http://msdn.microsoft.com/en-us/library/aa393907(v=vs.85).aspx
+		args = []interface{}{} //this works
+		//args = []interface{}{uint(1)} //this throws exception. exit code returned by terminated process
+		err = InvokeMethod(fmt.Sprintf("Win32_Process.Handle=%v", ProcessID), "Terminate", args, &ReturnValue)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Terminate ProcessID %v Result: %v", ProcessID, ReturnValue)
+
+	*/
+
+	//DSC Testing: requires WMF 5.0 preview. Test on ny-rdp01 or using above ps1 tests
+	//See MSFT_DSCLocalConfigurationManager mof file at "C:\Windows\System32\wbem\DscCore.mof" on ny-rdp01 for method signatures
+	//also http://www.codeproject.com/Articles/27914/WMI-MOF-Basics and wbemtest.exe see http://technet.microsoft.com/en-us/library/ee692770.aspx
+	ReturnValue = -100 //see http://msdn.microsoft.com/en-us/library/dn469252(v=vs.85).aspx
+	//[implemented,static : ToSubclass DisableOverride] uint32 GetMetaConfiguration([Out,EmbeddedInstance("MSFT_DSCMetaConfiguration") : ToSubclass] MSFT_DSCMetaConfiguration MetaConfiguration);
+	var MetaConfigurationRAW ole.VARIANT        //This may need to be an instance of MSFT_DSCMetaConfiguration instead of string pointer
+	args = []interface{}{&MetaConfigurationRAW} //return is EmbeddedInstance("MSFT_DSCMetaConfiguration") see http://msdn.microsoft.com/en-us/library/aa390377(v=vs.85).aspx
+	err = InvokeMethod("MSFT_DSCLocalConfigurationManager", "GetMetaConfiguration", args, &ReturnValue, nil, `root\Microsoft\Windows\DesiredStateConfiguration`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	MetaConfiguration := MetaConfigurationRAW.ToIDispatch() //NOTE: this was throwing "Object is not connected to server." until I commented out ole.CoUninitialize() in InvokeMethod. Need to call ole.CoInitializeEx outside InvokeMethod unless the variant type is converted to a native struct
+	FrequencyMins, err := oleutil.GetProperty(MetaConfiguration, "ConfigurationModeFrequencyMins")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("MSFT_DSCLocalConfigurationManager GetMetaConfiguration Result: %v FrequencyMins: %v", ReturnValue, FrequencyMins.Value())
+
+	ReturnValue = -100 //see http://msdn.microsoft.com/en-us/library/aa393907(v=vs.85).aspx
+	var resultRAW ole.VARIANT
+	//[implemented,static : ToSubclass DisableOverride] uint32 GetConfigurationStatus([In] uint32 Flags,[Out,EmbeddedInstance("MSFT_DSCConfigurationStatus") : ToSubclass] MSFT_DSCConfigurationStatus ConfigurationStatus[]);
+	args = []interface{}{nil, &resultRAW} //nil is input flags, but uint(0) doesnt work. 0 is flag for previous, 1 = ?? 2 = ??
+	err = InvokeMethod("MSFT_DSCLocalConfigurationManager", "GetConfigurationStatus", args, &ReturnValue, nil, `root\Microsoft\Windows\DesiredStateConfiguration`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//RawResult VT = VT_ARRAY | VT_VARIANT
+	resultRAWArray := resultRAW.ToArray() //result is an Array... we need the first result in the array converted to an IDispatch
+	resultRAWArrayType, _ := resultRAWArray.GetType()
+	resultRAWArrayDimensions, _ := resultRAWArray.GetDimensions()
+	resultRAWArraySize, _ := resultRAWArray.GetSize()
+	resultRAWArrayTotalElements, _ := resultRAWArray.TotalElements(0)
+	t.Logf("GetConfigurationStatus\nresultRAW: %#v \nArrayType: %#v \nArrayDim: %#v \nArraySize: %#v \nArrayTotal: %#v", resultRAW, resultRAWArrayType, resultRAWArrayDimensions, resultRAWArraySize, resultRAWArrayTotalElements)
+	var result []ole.VARIANT
+	err = resultRAWArray.ToVariantArray(&result)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%v cool", "we")
+	t.Logf("GetConfigurationStatus Result len %v cap %v", len(result), cap(result))
+	itemRAW := result[0]
+	t.Logf("GetConfigurationStatus itemRAW %#v", itemRAW)
+	item := itemRAW.ToIDispatch()
+	t.Logf("GetConfigurationStatus item %#v", item)
+	NumberOfResources, err := oleutil.GetProperty(item, "NumberOfResources") //item is wmi class MSFT_DSCConfigurationStatus
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("MSFT_DSCLocalConfigurationManager GetConfigurationStatus ReturnValue: %v NumberOfResources: %#v", ReturnValue, NumberOfResources.Value())
+
+	t.FailNow() //debuging: force showing of t.LogF messages while testing
+}
+
+type Win32_Process_Create struct {
+	ProcessId   uint32
+	ReturnValue uint32
+}
+
+type MSFT_DSCConfigurationStatus struct {
+}
+
+/* Disabled original tests and benchmarks to speed things up
 
 func TestQuery(t *testing.T) {
 	var dst []Win32_Process
@@ -19,6 +171,7 @@ func TestQuery(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
 
 func TestFieldMismatch(t *testing.T) {
 	type s struct {
@@ -151,6 +304,7 @@ func _TestMany(t *testing.T) {
 	}()
 	wg.Wait()
 }
+
 
 type Win32_Process struct {
 	CSCreationClassName        string
@@ -312,3 +466,5 @@ type Win32_OperatingSystem struct {
 	Version                                   string
 	WindowsDirectory                          string
 }
+
+*/
